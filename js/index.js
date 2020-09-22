dac2146fbc8437a4d772937d416d77405a9be708
Pierrick MERLANDE <merlandepierrick@gmail.com>
2020-09-22 15:49:26 +0200

    feat(change bot): change dynamically the bot when click

--- js/index.js
+++ js/index.js
@@ -6,119 +6,167 @@ index.js
 * Classes definition
 */
 class Message {
-	constructor(type, content) {
-		let date = new Date();
+  constructor(type, content) {
+    let date = new Date();
 
-    	this.type = type;
-    	this.content = content;
-    	this.time = date.getTime();
+      this.type = type;
+      this.content = content;
+      this.time = date.getTime();
   }
 }
 
 class Bot {
-	constructor(avatar, name) {
-		this.avatar = avatar;
-		this.name = name;
-		this.messages = [];
-	}
+  constructor(avatar, name) {
+    this.avatar = avatar;
+    this.name = name;
+    this.messages = [];
+  }
 }
 
 /*
 * Input messages functions
 */
 function getInputValue() {
-	let input = document.querySelector('.chat-input');
-	let inputValue = input.value;
+  let input = document.querySelector('.chat-input');
+  let inputValue = input.value;
 
-	input.value = "";
-	return inputValue;
+  input.value = "";
+  return inputValue;
 }
 
 function handleInput() {
-	let inputValue = getInputValue();
+  let inputValue = getInputValue();
 
-	if (inputValue != "") {
-		bots[actualBot].messages.push(new Message("messages-item--sent", inputValue));
-		/*
-		* here -> need to call bot function that will analyse the input and answer
-		* bots.answer(inputValue);
-		*	check inputValue
-		*	if it's "quelle heure est-il ?" -> bots.sayTime()
-		*		add a Message in bot messages -> type: messages-item--received / content: actual time
-		*/
-		displayMessages();
-	}
+  if (inputValue != "") {
+    bots[activeBot].messages.push(new Message("messages-item--sent", inputValue));
+    /*
+    * here -> need to call bot function that will analyse the input and answer
+    * bots.answer(inputValue);
+    *  check inputValue
+    *  if it's "quelle heure est-il ?" -> bots.sayTime()
+    *    add a Message in bot messages -> type: messages-item--received / content: actual time
+    */
+    displayMessages();
+  }
 }
 
 /*
 * Bots functions
 */
 function changeBot(event) {
-	let target = event.target;
+  let target = event.target;
+  let bot;
 
-	activeBot(target);
-	displayMessages();
+  if (target.parentNode.classList == "contact-users") {
+    bot = target;
+  } else {
+    bot = target.parentNode;
+  }
+
+  let botID = bot.id;
+  botID = botID.replace("botID", "");
+  activeBot = botID;
+
+  changeActiveBot(bot);
+  displayChatHeader();
+  displayMessages();
+  forceScroll();
 }
 
 /*
 * View functions
 */
 function forceScroll() {
-	let messagesContainer = document.querySelector(".messages-container");
-	messagesContainer.scrollTop = messagesContainer.scrollHeight;
+  let messagesContainer = document.querySelector(".messages-container");
+  messagesContainer.scrollTop = messagesContainer.scrollHeight;
 }
 
 function toggleSideBar() {
-	let contactContainer = document.querySelector(".contact-container");
+  let contactContainer = document.querySelector(".contact-container");
 
-	if (contactContainer.style.display == "block") {
-		contactContainer.style.display = "none";
-	} else {
-		contactContainer.style.display = "block";
-	}
+  if (contactContainer.style.display == "block") {
+    contactContainer.style.display = "none";
+  } else {
+    contactContainer.style.display = "block";
+  }
 }
 
 function displayMessages() {
-	let messagesContainer = document.querySelector('.messages-container')
-	let allMessages = bots[actualBot].messages.map(message => {
-		return `<div class="messages-item ${message.type}">
-            		<p class="messages-value">
-                    	${message.content}
+  let messagesContainer = document.querySelector('.messages-container')
+  let allMessages = bots[activeBot].messages.map(message => {
+    return `<div class="messages-item ${message.type}">
+                <p class="messages-value">
+                      ${message.content}
                     </p>
-              		<span class="messages-date">${message.time}</span>
+                  <span class="messages-date">${message.time}</span>
                 </div>`;
-	}).join('');
+  }).join('');
 
-	messagesContainer.innerHTML = allMessages;
+  messagesContainer.innerHTML = allMessages;
 }
 
-function activeBot(target) {
-	let bot;
+function changeActiveBot(bot) {
+  let activeBot = document.querySelector(".contact-users-active");
+  activeBot.classList.remove("contact-users-active");
+  bot.classList.add("contact-users-active");
+}
 
-	if (target.parentNode.classList == "contact-users") {
-		bot = target;
-	} else {
-		user = target.parentNode;
-	}
+function displayBots() {
+  let contactUsers = document.querySelector(".contact-users");
+  let id = 0;
 
-	let activeBot = document.querySelector(".contact-users-active");
-	activeBot.classList.remove("contact-users-active");
-	user.classList.add("contact-users-active");
+  let botsString = bots.map(bot => {
+    let active = "";
+
+    if (activeBot == id) {
+      active = "contact-users-active";
+    }
+    let str = `<div id="botID${id}" class="contact-users-item ${active}">
+                <div class="contact-users-avatar" style="background-image: url('${bot.avatar}')">    
+                </div>
+                <h3 class="contact-users-name">
+                  ${bot.name}
+                </h3>
+              </div>`;
+
+    id++;
+    return str;
+  }).join('');
+
+  contactUsers.innerHTML = botsString;
+}
+
+function displayChatHeader() {
+  let chatHeader = document.querySelector(".chat-header");
+  let avatar = bots[activeBot].avatar;
+  let name = bots[activeBot].name;
+  let templattedStr = `<div class="chat-header-avatar" style="background-image: url('${avatar}')">
+                      </div>
+                      <div class="chat-header-content">
+                        <h3 class="chat-header-name">${name}</h3>
+                        <h4 class="chat-header-online">
+                          Online now
+                        </h4>
+                      </div>`;
+
+  chatHeader.innerHTML = templattedStr;
 }
 
 /*
 * Initialisation
 */
 function initialisation() {
-	bots.push(new Bot("https://pbs.twimg.com/profile_images/1338985026/Picture_1_400x400.png", "Tyrion Lannister"));
-	bots.push(new Bot("https://i.pinimg.com/originals/fd/29/9c/fd299c3743a9679df23f110daf575ee4.jpg", "Arya Stark"));
+  bots.push(new Bot("https://pbs.twimg.com/profile_images/1338985026/Picture_1_400x400.png", "Tyrion Lannister"));
+  bots.push(new Bot("https://i.pinimg.com/originals/fd/29/9c/fd299c3743a9679df23f110daf575ee4.jpg", "Arya Stark"));
 
-	forceScroll();
-	displayMessages();
+  forceScroll();
+  displayBots();
+  displayChatHeader();
+  displayMessages();
 }
 
 var bots = []
-var actualBot = 0;
+var activeBot = 0;
 
 /*
 * Event listeners
